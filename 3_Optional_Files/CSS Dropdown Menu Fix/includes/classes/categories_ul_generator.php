1aff5ba26775f49b8a98ddab081c001440b8dd08
Erik <info@zen4all.nl>
2012-12-16 08:37:21 +0100

    updated for picalfours mega menu

--- 3_Optional_Files/CSS Dropdown Menu Fix/includes/classes/categories_ul_generator.php
+++ 3_Optional_Files/CSS Dropdown Menu Fix/includes/classes/categories_ul_generator.php
@@ -1,13 +1,12 @@
 <?php
-//
-// +----------------------------------------------------------------------+
+//placeholder21// +----------------------------------------------------------------------+
 // |zen-cart Open Source E-commerce                                       |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 2003 The zen-cart developers                           |
+// | Copyright(c) 2003 The zen-cart developers                           |
 // |                                                                      |
 // | http://www.zen-cart.com/index.php                                    |
 // |                                                                      |
-// | Portions Copyright (c) 2003 osCommerce                               |
+// | Portions Copyright(c) 2003 osCommerce                               |
 // +----------------------------------------------------------------------+
 // | This source file is subject to version 2.0 of the GPL license,       |
 // | that is bundled with this package in the file LICENSE, and is        |
@@ -24,24 +23,18 @@
 
 class zen_categories_ul_generator {
     var $root_category_id = CATEGORIES_ROOT,
-    $max_level = 7,
+    $max_level = 0,
     $data = array(),
-    $root_start_string = '',
-    $root_end_string = '',
-    $parent_start_string = '',
-    $parent_end_string = '',
-    
     $parent_group_start_string = '<ul%s>',
     $parent_group_end_string = '</ul>',
-    
     $child_start_string = '<li%s>',
     $child_end_string = '</li>',
-    
     $spacer_string = '
-  ',
+',
     $spacer_multiplier = 1;
     
-    var $document_types_list = ' (3) ';// acceptable format example: ' (3, 4, 9, 22, 18) '
+    var $document_types_list = ' (3) ';
+    // acceptable format example: ' (3, 4, 9, 22, 18) '
     
     function zen_categories_ul_generator($load_from_database = true)
     {
@@ -61,55 +54,30 @@ and c.categories_status=1 " .
         }
     }
     
-    function buildBranch($parent_id, $level = 0, $submenu=false, $parent_link='')
-    {    
+    function buildBranch($parent_id, $level, $submenu=true, $parent_link='')
+    {
         $result = sprintf($this->parent_group_start_string, ($submenu==true) ? ' class="level'. ($level+1) . '"' : '' );
         
         if (($this->data[$parent_id])) {
             foreach($this->data[$parent_id] as $category_id => $category) {
-                 $category_link = $parent_link . $category_id; 
-        
-        if (($this->data[$category_id])) {
-        $result .= sprintf($this->child_start_string, ($submenu==true) ? ' class="submenu"' : '');
-        } 
-        
-        else {
-                     if (($this->data[$category_id]) && ($submenu==false)) {
-                          $result .= sprintf($this->parent_group_start_string, ($submenu==true) ? ' class="level'. ($level+1) . '"' : '');
-                          $result .= sprintf($this->child_start_string, ($submenu==true) ? ' class="submenu"' : '');
-                    
-                            } else {
-                              $result .= sprintf($this->child_start_string, '');
-                            }
-              
-            }
-                
-                if ($level == 0) {
-                    $result .= $this->root_start_string;
+                $category_link = $parent_link . $category_id;
+                if (($this->data[$category_id])) {
+                    $result .= sprintf($this->child_start_string, ($submenu==true) ? ' class="submenu"' : '');
+                } else {
+                    $result .= sprintf($this->child_start_string, '');
                 }
                 $result .= str_repeat($this->spacer_string, $this->spacer_multiplier * 1) . '<a href="' . zen_href_link(FILENAME_DEFAULT, 'cPath=' . $category_link) . '">';
                 $result .= $category['name'];
                 $result .= '</a>';
-                
-                if ($level == 0) {
-                    $result .= $this->root_end_string;
-                }
-                
-                if (($this->data[$category_id])) {
-                    $result .= $this->parent_end_string;
-                }
-                
+
                 if (($this->data[$category_id]) && (($this->max_level == '0') || ($this->max_level > $level+1))) {
                     $result .= $this->buildBranch($category_id, $level+1, $submenu, $category_link . '_');
-                    
                 }
                 $result .= $this->child_end_string;
-                
             }
         }
         
         $result .= $this->parent_group_end_string;
-        
         return $result;
     }
     
