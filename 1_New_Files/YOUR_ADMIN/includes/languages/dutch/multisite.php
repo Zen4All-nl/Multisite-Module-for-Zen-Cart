2c63b7bebca5e7bc4c81f11f1d7270e6e0f9b141
Erik <info@zen4all.nl>
2013-01-21 10:04:42 +0100

    translated

--- 1_New_Files/YOUR_ADMIN/includes/languages/dutch/multisite.php
+++ 1_New_Files/YOUR_ADMIN/includes/languages/dutch/multisite.php
@@ -6,9 +6,9 @@
 define('MULTISITE_TITLE','Multisite');
 define('MULTISITE_CONFIG_LINK','Toon configuratie voor een nieuwe site');
 define('MULTISITE_RELATIONS_LINK','Toon de relaties tussen Categorieën / Sites');
-define('MULTISITE_CONFIG_TEXT','List of the config that you can override from a config file from includes/config_sites/*_config.php<br />You don\'t need to copy all of them, you can only copy the one you wish to modify.');
+define('MULTISITE_CONFIG_TEXT','Een lijst van de config waarvoor je een override kunt opgeven vanuit includes/config_sites/*_config.php<br />Het is niet nodig om alles te kopieren, alleen de delen die je wilt aanpassen is genoeg.');
 define('MULTISITE_CATEGORIE_TITLE','Categorieën / Sites');
-define('MULTISITE_CATEGORIE_TEXT','You can use this tool in order to allocate a category to a site.<br/>All you have to do is to write the SITE_NAME (defined in the folder config_sites) in the collomn Sites.<br/>If a categories has to be seen by more than one site, then you have to write the sites separated by a dash.<br/>Voorbeeld: site1-site2-site3<br/><br/>If you want a sub-category  to be seen on a site, you should also add its parent category.');
+define('MULTISITE_CATEGORIE_TEXT','Deze tool kan gebruikt worden om een categorie toe te wijzen aan een site.<br/>Je hoeft alleen maar de SITE_NAME (gedefiniëerd in de folder config_sites) in de kolom Sites in te vullen.<br/>Als de categorie in meer dan één site te zien moet zijn, dan moet de sitenaam gescheiden worden door een streepje ( - ).<br/>Voorbeeld: site1-site2-site3<br/><br/>Voor subcategorieën, moet ook de voorgaande categorie opgegeven worden.');
 define('MULTISITE_CATEGORIE_ADD_LABEL','Voeg deze site aan alle categorieën toe');
 define('MULTISITE_BUTTON_ADD_ALL','Aan alles toevoegen');
 define('MULTISITE_BUTTON_REMOVE_ALL','Uit alles verwijderen');
